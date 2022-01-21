3869b578cfbc1eb86c4a64ec501b77b3299fbf10
Abdulhakeem Almidan <Hakeemmidan@gmail.com>
2022-01-21 18:56:12 +0300

    Change app.config.js scheme key to fit app name

--- app.cnofig.js
+++ app.cnofig.js
@@ -7,7 +7,7 @@ export default {
     version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/images/icon.png',
-    scheme: 'myapp',
+    scheme: 'grapes1',
     userInterfaceStyle: 'automatic',
     splash: {
       image: './assets/images/splash.png',
